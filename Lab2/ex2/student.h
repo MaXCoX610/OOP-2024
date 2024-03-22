93e9b2206479fbb1d438cf9979145430151ac21e
MaXCoX610 <154246298+MaXCoX610@users.noreply.github.com>
2024-03-22 16:08:28 +0200

    started ex2

---
+++ Lab2/ex2/student.h
@@ -0,0 +1,11 @@
+class student
+{
+    char name[101];
+    int count;
+public:
+    void GetName();          // student name
+    void SetMathGreade();
+    void GetMathGrade();     
+    void Sort();          // will sort the numbers vector
+    void Print();         // will print the current vector
+};
