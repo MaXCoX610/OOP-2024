1fee6d89140bb1d53eb2dbb23798d38d9ac6efad
MaXCoX610 <154246298+MaXCoX610@users.noreply.github.com>
2024-03-22 19:35:38 +0200

    Revert "ups"
    This reverts commit 6904b82fef50fd77caae531a6f08397e08c22b37.

---
+++ Lab2/ex1/main.cpp
@@ -0,0 +1,19 @@
+
+#include "NumberList.h"
+using namespace std;
+int main(){
+    NumberList n;
+    n.Init();
+    for(int i=0; i<10; i++)
+        if(i%2==0)
+            n.Add(i*10-i*15);
+        else
+            n.Add(i*10);
+    if(!n.Add(110))
+        cout<<"Cannot add as the array is full"<<'\n';
+    n.Print();
+    cout<<'\n';
+    n.Sort();
+    n.Print();
+    return 0;
+}
